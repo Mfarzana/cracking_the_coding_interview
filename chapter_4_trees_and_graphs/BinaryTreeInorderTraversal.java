1939885cc4a47f65c11eb5995afd6ed72486d4f8
Mfarzana <mfarzana1133@gmail.com>
2020-11-03 22:56:08 +0900

    Binary Tree Inorder Traversal

---
+++ chapter_4_trees_and_graphs/BinaryTreeInorderTraversal.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data_structure;
+
+import java.util.Stack;
+
+/**
+ *
+ * @author Farzana
+ */
+/*
+     1
+    /  \
+   2     3
+ /   \
+4   5
+Inorder (Left, Root, Right) : 4 2 5 1 3
+*/
+
+public class BinaryTreeInorderTraversal {
+       
+   // Recursion
+    // solution 1
+     public static void inorderTraversal(TreeNode node){
+         if(node==null) return ;
+         inorderTraversal(node.left);
+         System.out.print("  "+node.val+"  ");
+         inorderTraversal(node.right);
+         
+     }
+   //  solution 2
+      public static void solution2(TreeNode node){
+            if(node!=null) {
+               inorderTraversal(node.left);
+               System.out.print("  "+node.val+"  ");
+               inorderTraversal(node.right);
+
+           }
+      }
+        //  solution 3 without recursion
+      public static void solution3(TreeNode node){
+            Stack<TreeNode> stack=new Stack();
+            
+      }
+     
+        
+        public static void main(String args[]){
+            
+            TreeNode node=new TreeNode(1);
+            node.left=new TreeNode(2);
+            node.right=new TreeNode(3);
+            node.left.left=new TreeNode(4);
+            node.left.right=new TreeNode(5);
+            
+            BinaryTreeInorderTraversal.inorderTraversal(node);
+             System.out.println();
+            BinaryTreeInorderTraversal.solution2(node);
+            
+        }
+}
+
+class TreeNode{
+                int val;
+                TreeNode left;
+                TreeNode right;
+
+                public TreeNode(int val){
+                    this.val=val;
+                    this.left=null;
+                    this.right=null;
+                }
+    }
